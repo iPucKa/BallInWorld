bcc58e2abd472b7178f4b2e0045952d984c9c9da
iPucKa <fourtails@mail.ru>
2025-03-21 21:15:12 +0300

    Scene1 Done

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+	private bool _isCollected;
+	public bool IsCollected => _isCollected;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Ball>() != null)
+		{
+			_isCollected = true;
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private Ball _ball;
+	[SerializeField] private List<Coin> _coinsInScene;
+
+	[SerializeField] private float _maxTimeToLose;
+	
+	private int _coinsCountToWin;
+
+	private float _currentTime;
+
+	private bool _isPlaying;
+
+	private void Awake()
+	{
+		_coinsCountToWin = _coinsInScene.Count;
+
+		_isPlaying = true;
+		_currentTime = 0;
+	}
+
+	private void Update()
+	{
+		if (_isPlaying)
+		{
+			CheckGameState();
+		}
+	}
+
+	private void CheckGameState()
+	{
+		CheckTime(FindRemainingTime());
+
+		CheckCoinsNumber(CalculateCoins());
+	}
+
+	private void CheckTime(float remainingTime)
+	{
+		if (remainingTime <= 0)
+		{
+			Debug.Log("ÂÛ ÏÐÎÈÃÐÀËÈ!");
+			StopGame();
+		}
+	}
+
+	private float FindRemainingTime()
+	{
+		_currentTime += Time.deltaTime;
+		float remainingTime = _maxTimeToLose - _currentTime;
+
+		Debug.Log($"Äî êîíöà èãðû îñòàëîñü: {remainingTime.ToString("0.000")} c");
+		return remainingTime;
+	}
+
+	private int CalculateCoins()
+	{
+		if (_coinsInScene == null)
+			return 0;
+
+		int collectedCoins = 0;
+
+		foreach (Coin coin in _coinsInScene)
+		{
+			if (coin.IsCollected)
+				collectedCoins++;
+		}
+
+		return collectedCoins;
+	}
+
+	private void CheckCoinsNumber(int countCoins)
+	{
+		if (countCoins == _coinsCountToWin)
+		{
+			Debug.Log("ÂÛ ÏÎÁÅÄÈËÈ!");
+			StopGame();
+		}
+	}
+
+	private void StopGame()
+	{
+		_ball.StopBall();
+
+		_isPlaying = false;
+	}
+}
